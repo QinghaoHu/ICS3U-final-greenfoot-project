89433e0b5529c03b767d4d1278156c65a93a1b5e
QinghaoHu <huqinghao@outlook.com>
2025-01-15 15:39:49 -0500

    update

--- source/userTankBody.java
+++ source/userTankBody.java
@@ -11,36 +11,71 @@ public class userTankBody extends SuperSmoothMover {
      * Act - do whatever the userTank wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int hp;
+    private int movingSpeed;
+    private int tankSoundIndex;
+    
+    private GreenfootSound[] tankSound;
+    
     public userTankBody() {
         setImage("userTankBody.png");
         getImage().rotate(90);
+        hp = 1000;
+        tankSoundIndex = 0;
+        tankSound = new GreenfootSound[2];
+        for (int i = 0; i < 2; i++) {
+            tankSound[i] = new GreenfootSound("tankDriving.wav");
+            tankSound[i].setVolume(75);
+        }
     }
     
     public void act(){
+        checkKey();
+    }
+    
+    public void checkKey() {
+        boolean isMove = false;
         if (Greenfoot.isKeyDown("w")) {
             move(2); // Move forward
+            isMove = true;
         } else if (Greenfoot.isKeyDown("s")) {
             move(-2); // Move backward
+            isMove = true;
         }
-
         // Rotate the tank body with left/right arrow keys (optional)
         if (Greenfoot.isKeyDown("a")) {
             turn(-2); // Rotate counter-clockwise
+            isMove = true;
         } else if (Greenfoot.isKeyDown("d")) {
             turn(2); // Rotate clockwise
+            isMove = true;
         }
         
         if (Greenfoot.isKeyDown("up")) {
             move(2); // Move forward
+            isMove = true;
         } else if (Greenfoot.isKeyDown("down")) {
             move(-2); // Move backward
+            isMove = true;
         }
 
         // Rotate the tank body with left/right arrow keys (optional)
         if (Greenfoot.isKeyDown("left")) {
             turn(-2); // Rotate counter-clockwise
+            isMove = true;
         } else if (Greenfoot.isKeyDown("right")) {
             turn(2); // Rotate clockwise
+            isMove = true;
+        }
+        
+        if (isMove) {
+            playSound();
         }
     }
+    
+    public void playSound() {
+        tankSound[tankSoundIndex % 2].play();
+        tankSoundIndex++;
+    }
+    
 }
